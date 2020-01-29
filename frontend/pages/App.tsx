9e0118cbc5285453bdb8a5363ecd8b45bec73c57
Spencer Duball <spencerduball@gmail.com>
2020-01-28 19:51:09 -0600

    Reverted back to styled-components 4, issue with 5

--- frontend/pages/App.tsx
+++ frontend/pages/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import App from "next/app";
+import { ThemeProvider } from "styled-components";
+import lightTheme from "../src/styled/lightTheme";
 
-// load css files
+// load global stylesheets
+import "../node_modules/normalize.css/normalize.css";
+import "../src/css/global.css";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <>
+      <ThemeProvider theme={lightTheme}>
         <Component {...pageProps} />
-      </>
+      </ThemeProvider>
     );
   }
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
-  background-color: red;
+  background-color: blue;
 `;
 
 const HomePage = () => {
