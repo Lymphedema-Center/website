82c1995f37f699bec941cce858ec466d302c4ec3
Spencer Duball <spencer-duball@uiowa.edu>
2020-01-29 14:02:17 -0600

    Added backgrounds to Storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-knobs/register",
-    "@storybook/addon-viewport/register"
+    "@storybook/addon-viewport/register",
+    "@storybook/addon-backgrounds/register"
   ],
   presets: [path.resolve(__dirname, "./next-preset.js")]
 };
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -4,5 +4,9 @@ import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 addParameters({
   viewport: {
     viewports: INITIAL_VIEWPORTS
-  }
+  },
+  backgrounds: [
+    { name: "twitter", value: "#00aced", default: true },
+    { name: "facebook", value: "#3b5998" }
+  ]
 });
