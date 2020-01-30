7179c703e5102dc498c34284a0968a640eda49db
Spencer Duball <spencerduball@gmail.com>
2020-01-30 10:26:10 -0600

    Added code coverage reporting to cypress

--- frontend/cypress/support/index.js
+++ frontend/cypress/support/index.js
@@ -14,7 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// import code-coverage commands
+import "@cypress/code-coverage/support";
