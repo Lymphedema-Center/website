b2524eb10b09c96d74ab8a914413ade5b2b744f9
Spencer Duball <spencerduball@gmail.com>
2020-02-08 21:18:49 -0600

    Fixed all tests, -> 100%

--- frontend/cypress/tests/unit/svg/brand/LymphedemaCenterLogo.spec.tsx
+++ frontend/cypress/tests/unit/svg/brand/LymphedemaCenterLogo.spec.tsx
@@ -2,8 +2,42 @@ import React from "react";
 import LymphedemaCenterLogo from "../../../../../components/svg/brand/LymphedemaCenterLogo";
 
 describe("LymphedemaCenterLogo component", function() {
-  it("mounts", function() {
-    // mount the component
-    cy.mount(((<LymphedemaCenterLogo />) as unknown) as Symbol);
+  describe("props", () => {
+    const width = "1049px";
+    const height = "371px";
+
+    it("only width supplied", function() {
+      cy.mount(
+        ((
+          <LymphedemaCenterLogo width={width} data-cy="LymphedemaCenterLogo" />
+        ) as unknown) as Symbol
+      );
+      cy.get("[data-cy=LymphedemaCenterLogo]")
+        .should("have.css", "width", width)
+        .and("have.css", "height", height);
+    });
+
+    it("only height supplied", function() {
+      cy.mount(
+        ((
+          <LymphedemaCenterLogo
+            height={height}
+            data-cy="LymphedemaCenterLogo"
+          />
+        ) as unknown) as Symbol
+      );
+      cy.get("[data-cy=LymphedemaCenterLogo]")
+        .should("have.css", "width", width)
+        .and("have.css", "height", height);
+    });
+
+    it("neither supplied", function() {
+      // if neither, still verify mount
+      cy.mount(
+        ((
+          <LymphedemaCenterLogo data-cy="LymphedemaCenterLogo" />
+        ) as unknown) as Symbol
+      );
+    });
   });
 });
