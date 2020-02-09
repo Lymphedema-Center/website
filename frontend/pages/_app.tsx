4ca3c196aba91b5e0e6cbf355296731368cd98c0
Spencer Duball <spencerduball@gmail.com>
2020-02-09 12:33:29 -0600

    Added general layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 import App from "next/app";
+import Header from "../components/composed/Header";
 import { ThemeProvider } from "styled-components";
 import lightTheme from "../components/styled/lightTheme";
 
@@ -7,12 +9,30 @@ import lightTheme from "../components/styled/lightTheme";
 import "../node_modules/normalize.css/normalize.css";
 import "../css/global.css";
 
+const PageContainer = styled.div`
+  display: grid;
+  grid-template-rows: max-content 1fr max-content;
+  min-height: 100%;
+`;
+
+const FixedHeader = styled(Header)`
+  position: fixed;
+`;
+const InvisHeader = styled(Header)`
+  visibility: hidden;
+`;
+
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={lightTheme}>
-        <Component {...pageProps} />
+        <FixedHeader hamburgerOnClick={() => {}} />
+        <PageContainer>
+          <InvisHeader hamburgerOnClick={() => {}} />
+          <Component {...pageProps} />
+        </PageContainer>
+        <Header hamburgerOnClick={() => {}} />
       </ThemeProvider>
     );
   }
